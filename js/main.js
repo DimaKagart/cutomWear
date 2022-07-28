8e35462ac4b6d342486c7cf7ce16956dfb890cfc
DimaKagart <109988708+DimaKagart@users.noreply.github.com>
2022-07-28 11:47:35 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".owl-one").owlCarousel({
+        center: true,
+        loop: true,
+        items: 4,
+        margin: -40,
+        dotsEach: true,
+    });
+  });
+
+  $(document).ready(function(){
+    $(".owl-two").owlCarousel({
+        center: true,
+        loop: true,
+        items: 6,
+        margin: 130,
+        dotsEach: true,
+    });
+  });
